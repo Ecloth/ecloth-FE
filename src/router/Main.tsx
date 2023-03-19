d876217d9e8fac8677da6506dcb7c4258a73cc7e
ulimsss <z_o__o@naver.com>
2023-03-19 15:06:24 +0900

    chore: 초기 환경 세팅 수정 및 라우터 파일 변경

--- src/router/Main.tsx
+++ src/router/Main.tsx
@@ -1,17 +1,35 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import styled from "styled-components";
+import DetailModal from "../components/detailPost/DeatilModal";
+import Footer from "../layouts/Footer";
 import Nav from "../layouts/Nav";
+import Chat from "../pages/chat/Chat";
+import ChatId from "../pages/chat/ChatId";
 import Feed from "../pages/feed/Feed";
+import FeedPage from "../pages/feed/FeedPage";
+import Edit from "../pages/profile/Edit";
 
 const Main = () => {
+  const location = useLocation();
+
+  const background = location.state && location.state.background;
+
   return (
     <>
       <Nav />
       <Section />
       <Routes>
         {/* 메인 / 커뮤니티 / 채팅 페이지 들어가는 부분 */}
+        <Route path="/profile/edit" element={<Edit />} />
+        <Route path="/profile/*" element={<Edit />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/myPage/:id" element={<FeedPage />} />
+        <Route path="/feed/*" element={<Feed />} />
+        <Route path="/feed/:id" element={<DetailModal />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/chat/:id" element={<ChatId />} />
       </Routes>
+      <Footer />
     </>
   );
 };
