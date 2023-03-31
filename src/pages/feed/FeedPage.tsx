48fab1d2ec784f2695653d72d176ac3cf43aaf15
ulimsss <z_o__o@naver.com>
2023-03-31 13:06:23 +0900

    feat: feed page API 코드 추가

--- src/pages/feed/FeedPage.tsx
+++ src/pages/feed/FeedPage.tsx
@@ -1,16 +1,12 @@
-import {useParams} from "react-router-dom";
-import SectionWrapper from "../../components/commons/SectionWrapper";
-import {dummy} from "../../components/feed/FeedBody";
-import MainFeed from "../../components/myPage/MainFeed";
-import UserInfo from "../../components/myPage/UserInfo";
+import SectionWrapper from '../../components/commons/SectionWrapper';
+import MainFeed from '../../components/myPage/MainFeed';
+import UserInfo from '../../components/myPage/UserInfo';
 
 function FeedPage() {
-  const {id} = useParams();
-  const posts = dummy.filter(item => id === item.member_id);
   return (
     <SectionWrapper>
       <UserInfo />
-      <MainFeed posts={posts} />
+      <MainFeed />
     </SectionWrapper>
   );
 }
