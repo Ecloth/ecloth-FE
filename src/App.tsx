fc63d20457809e81de4399cdd3167135ece7ec14
Logical_IT <>
2023-04-06 12:49:10 +0900

    feat : app 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Router from "./router/Router";
 import "./App.css";
+export const TEST_MEMBER_ID = 7;
 export const TEST_TOKEN = 
-"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4IiwiUk9MRSI6WyJST0xFX01FTUJFUiJdLCJpYXQiOjE2ODA3MDU1NTQsImV4cCI6MTY4MDcwNzM1NH0.CmCmyahkbRoPYpx5J1yHXZiE6Bk_qNpOcT9VAQQQvS4"
+"Bearer " + localStorage.getItem('token')
 function App() {
 
   return <Router />;
