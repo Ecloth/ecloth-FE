ccca844a8150110da556ca1c229f66a89142208e
Logical_IT <>
2023-04-06 12:15:11 +0900

    feat : 프로필 편집 페이지 api 연동 코드 추가

--- src/components/profileEdit/EditBody.tsx
+++ src/components/profileEdit/EditBody.tsx
@@ -1,10 +1,171 @@
 import styled from 'styled-components';
 import profile from '../../assets/images/profile.png';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
+import { EmailState, NicknameState, RoleState } from '../../atoms/Atom';
+import Swal from 'sweetalert2';
 
 
 function EditBody() {
-  const info : string | any = localStorage.getItem("email")
+  const [nick, setNick] = useRecoilState<string | any>(NicknameState)
+  const token = localStorage.getItem('token')
+  const [email, setEmail] = useState<string>()
+  const [Password, setPassword] = useState<string>()
+  const [newPassword, setNewPassword] = useState<string>()
+  const [newConfirmPassword, setNewConfirmPaasword] = useState<string>()
+  const [nickname, setNickname] = useState<string>()
+  const [phone, setPhone] = useState<string | any>()
+  const [profileImagePath, setProfileImagePath] = useState<string | any>({selectedFiles : null})
+  const [role, setRole] = useRecoilState<boolean>(RoleState)
+  const [emailConfirm, setEmailConfirm] = useState<string>()
 
+  const handleNicknameInput = (e : ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.currentTarget.value)
+  }
+  const handlePasswordInput = (e : ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setPassword(e.currentTarget.value)
+  }
+  const handleNewPasswordInput = (e : ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setNewPassword(e.currentTarget.value)
+  }
+  const handleNewConfirmPasswordInput = (e : ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setNewConfirmPaasword(e.currentTarget.value)
+  }
+  const handlePhoneInput = (e : ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.currentTarget.value)
+    if(phone.length === 11) {
+      setPhone(phone.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'))
+    }
+  }
+  const handleProfileImageInput = (e : ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    setProfileImagePath({
+      selectedFiles : files
+    })
+  }
+  const handleModifyButton = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      axios({
+        // url: 'https://43cb-175-194-251-236.jp.ngrok.io/api/member/me',
+        url: 'http://13.125.74.102:8080/api/member/me',
+        headers : {
+          'Authorization' : `Bearer ` + token
+        },
+        params : {
+          newPassword : newPassword,
+          nickname : nickname,
+          Password : Password,
+          phone : phone,
+          profileimagepath : profileImagePath,
+        },
+        method: 'PUT',
+        withCredentials: true,
+      })
+        .then((result) => {
+          console.log("Modify", result)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      fetch(`http://13.125.74.102:8080/api/register/email-auth?code=${emailConfirm}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    Swal.fire({
+      title: '회원정보를 수정하시겠습니까?',
+      text: "다시 수정할 수 있습니다.",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '확인',
+      cancelButtonText: '취소'
+  }).then((result) => {
+      if (result.isConfirmed) {
+          Swal.fire(
+              "수정 중입니다."
+              )
+            }
+            // window.location.replace("/profile/edit")
+});
+  }
+
+  const handleNicknameDuplicationButton = () => {
+    fetch('http://13.125.74.102:8080/api/register', {
+    // fetch('https://43cb-175-194-251-236.jp.ngrok.io/api/register', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify({
+      nickname : nickname,  // 백엔드에서 key를 뭐로 받는지 확인하고 작성 
+    }),
+  })
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data.message)
+      if(data.message === "이미 사용중인 닉네임 입니다.") {
+        Swal.fire({
+          text: '이미 사용중인 닉네임 입니다.',
+          width: 350,
+          padding: 10,
+          confirmButtonText: '확인',
+        })} else if("" === nickname){
+          Swal.fire({
+            text: '닉네임을 입력해주세요.',
+            width: 350,
+            padding: 10,
+            confirmButtonText: '확인',
+          })} else {
+            Swal.fire({
+              text: '사용가능한 닉네임 입니다.',
+              width: 350,
+              padding: 10,
+              confirmButtonText: '확인',
+            })}
+    });
+ }
+
+  const handleEmailConfirm = (e : ChangeEvent<HTMLInputElement>) => {
+    setEmailConfirm(e.currentTarget.value)
+  }
+  console.log(emailConfirm)
+
+  useEffect(() => {
+    try {
+      axios({
+        url: 'http://13.125.74.102:8080/api/member/me',
+        headers : {
+          'Authorization' : `Bearer ` + token
+        },
+        method: 'GET',
+        withCredentials: true,
+      })
+        .then((result) => {
+          console.log("me", result)
+          if(result.data.role === 'ROLE_MEMBER'){
+            setRole(true)
+          }
+          setEmail(result.data.email)
+          setPhone(result.data.phone)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  },[])
   return (
     <BodyWrapper>
       <Title>프로필 편집</Title>
@@ -13,25 +174,27 @@ function EditBody() {
           <ProfileEditSubImg src={profile}></ProfileEditSubImg>
         </ProfileEditSubDiv>
         <NameDiv>
-          <Name>{info}</Name>
-          <ProfileImgEdit >프로필 사진 바꾸기</ProfileImgEdit>
+          <Name>{nick}</Name>
+          {/* <ProfileImgEdit >프로필 사진 바꾸기</ProfileImgEdit> */}
+          <ProfileImgEdit type='file' onChange={handleProfileImageInput}/>
         </NameDiv>
       </ProfileEditDiv>
-      <PersonalinfoForm>
+      <PersonalinfoForm onSubmit={handleModifyButton}>
         <Div>
           <Aside>
             <Label>닉네임</Label>
           </Aside>
           <InputDiv>
-          <Input placeholder={info}/>
+          <Input placeholder={nick} onChange={handleNicknameInput}/>
           </InputDiv>
         </Div>
+          <Duplication type='button' onClick={handleNicknameDuplicationButton}>중복확인</Duplication>
         <Div>
         <Aside>
             <Label>전화번호</Label>
           </Aside>
           <InputDiv>
-          <Input placeholder='010-0000-0000'/>
+          <Input type="text" placeholder={phone} onChange={handlePhoneInput}/>
           </InputDiv>
         </Div>
         <Div>
@@ -39,34 +202,45 @@ function EditBody() {
             <Label>이메일</Label>
           </Aside>
           <InputDiv>
-          <Input placeholder={info}/>
+          <Input placeholder={email} value={email}/>
           </InputDiv>
         </Div>
-        <Div>
-        <Aside>
-            <Label>이전 비밀번호</Label>
-          </Aside>
-          <InputDiv>
-          <Input />
-          </InputDiv>
-        </Div>
-        <Div>
-        <Aside>
-            <Label>새 비밀번호</Label>
-          </Aside>
-          <InputDiv>
-          <Input />
-          </InputDiv>
-        </Div>
-        <Div>
-        <Aside>
-            <Label>새 비밀번호 확인</Label>
-          </Aside>
-          <InputDiv>
-          <Input />
-          </InputDiv>
-        </Div>
-      <EditButton>수정하기</EditButton>
+        {role &&
+        <><Div>
+            <Aside>
+              <Label>이전 비밀번호</Label>
+            </Aside>
+            <InputDiv>
+              <Input type='password' value={Password} onChange={handlePasswordInput}/>
+            </InputDiv>
+          </Div>
+          <Div>
+              <Aside>
+                <Label>새 비밀번호</Label>
+              </Aside>
+              <InputDiv>
+                <Input type='password' value={newPassword} onChange={handleNewPasswordInput} />
+              </InputDiv>
+            </Div>
+            <Div>
+              <Aside>
+                <Label>새 비밀번호 확인</Label>
+              </Aside>
+              <InputDiv>
+                <Input type='password' value={newConfirmPassword} onChange={handleNewConfirmPasswordInput}/>
+              </InputDiv>
+            </Div>
+            <Div>
+              <Aside>
+                <Label> 이메일 인증 </Label>
+              </Aside>
+              <InputDiv>
+                <Input type='text' value={emailConfirm} onChange={handleEmailConfirm}/>
+              </InputDiv>
+            </Div>
+            </>
+        }
+      <EditButton type='submit'>수정하기</EditButton>
       </PersonalinfoForm>
     </BodyWrapper>
   );
@@ -93,7 +267,7 @@ const Title = styled.div`
   width: 200px;
   height: 50px;
   text-align: center;
-  margin-top: 20px;
+  margin-top: 10px;
   margin-left: 20px;
   font-size: 30px;
   background-color: #6aafe6;
@@ -114,10 +288,36 @@ const ProfileEditDiv = styled.div`
   font: inherit;
   font-size: 100%;
   justify-content: flex-start;
-  margin: 32px 0 0;
+  margin: 10px 70px 0;
   padding: 0;
   position: relative;
   vertical-align: baseline;
+`;
+
+const Duplication = styled.button`
+  position : absolute;
+  margin-top: 3px;
+  display: inline-flex;
+  align-items: center;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  right : 0px;
+  top: 124px;
+  height: 30px;
+  font-size: 12px;
+  background: #228be6;
+  &:hover {
+    background: #339af0;
+  }
+  &:active {
+    background: #1c7ed6;
+  }
 `;
 
 const ProfileEditSubDiv = styled.div`
@@ -170,7 +370,7 @@ const Name = styled.div`
   display: block;
 `;
 
-const ProfileImgEdit = styled.button`
+const ProfileImgEdit = styled.input`
   font-weight: 600;
   height: auto;
   display: inline;
@@ -230,7 +430,7 @@ const PersonalinfoForm = styled.form`
   flex-direction: column;
   font: inherit;
   font-size: 100%;
-  margin: 0;
+  margin: 75px;
   margin-bottom: 16px;
   margin-top: 16px;
   padding: 0;
@@ -271,7 +471,7 @@ const InputDiv = styled.div`
   border: 0;
   box-sizing: border-box;
   color: rgb(38 38 38);
-  display: flex;
+  /* display: flex; */
   flex-direction: column;
   flex-grow: 1;
   flex-shrink: 0;
@@ -294,7 +494,7 @@ const Input = styled.input`
     font-size: 16px;
     height: 32px;
     padding: 0 10px;
-    width: 100%;
+    width: 90%;
     `
 
 const EditButton = styled.button`
