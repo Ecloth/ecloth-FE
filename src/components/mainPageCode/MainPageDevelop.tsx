5bc964acd37139d4ba31a6157aa9c42d16278a89
Logical_IT <>
2023-04-10 17:03:34 +0900

    feat : 메인페이지 코드 수정

--- src/components/mainPageCode/MainPageDevelop.tsx
+++ src/components/mainPageCode/MainPageDevelop.tsx
@@ -1,8 +1,196 @@
-import React from 'react'
+import styled from 'styled-components';
+import React, { useState } from "react";
+import Nav from "../../layouts/Nav";
+import Navv from "../../layouts/Navv";
+import { useRecoilState } from "recoil";
+import {
+  CurrentXState,
+  CurrentYState,
+  GridXState,
+  GridYState,
+  ImageState,
+  NicknameState,
+} from "../../atoms/Atom";
+import MapSearchCode from "./MapSearchCode";
+import Temperature from "../../pages/main/Temperature";
+import WeatherGraph from '../../pages/main/WeatherGraph';
+import SeasonBg from '../../pages/main/SeasonBg';
+import ClothPage from '../../pages/main/ClothPage';
 
 export default function MainPageDevelop() {
+  const [latitude, setLatitude] = useState<number | any>();
+  const [longitude, setLongitude] = useState<number | any>();
+  const [gridX, setGridX] = useRecoilState<number | any>(GridXState);
+  const [gridY, setGridY] = useRecoilState<number | any>(GridYState);
+  const [currentX, setCurrentX] = useRecoilState<number | any>(CurrentXState);
+  const [currentY, setCurrentY] = useRecoilState<number | any>(CurrentYState);
+  const [nick, setNick] = useRecoilState<string | any>(NicknameState);
+  const [image, setImage] = useRecoilState(ImageState);
+  setNick("장기철");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    setLatitude(latitude);
+    setLongitude(longitude);
+  });
+
   return (
     <>
+      <div class="flex w-full bg-white">
+        {/* <Navv />   */}
+        <main class="min-h-screen border-l bg-white">
+          <nav class="flex items-center justify-between border-b bg-white px-10 py-6">
+            <div class="flex w-5/6 items-center">
+              <MapSearchCode latitude={latitude} longitude={longitude} />
+            </div>
+            <div class="flex items-center space-x-4">
+              <img class="w-8 rounded-full" src={image} alt="Image" />
+              <p class="hidden md:block">{nick}</p>
+            </div>
+          </nav>
+          <div class="mx-6">
+            {/* <h1 class="my-6 text-3xl">날씨</h1> */}
+
+            <div class="mt-4 space-y-3 md:flex md:space-y-0">
+              <div class="ml-auto h-25 rounded-md bg-gradient-to-r bg-sky-400 p-3 absolute right-14 top-40">
+                  <Temperature
+                    latitude={currentY}
+                    longitude={currentX}
+                    gridX={gridX}
+                    gridY={gridY}
+                  />
+            </div>
+                <GraphDiv>
+                <WeatherGraph />
+                </GraphDiv>
+                <SeasonBg />
+            </div>
+          </div>
+
+          <div class="mx-6">
+
+            <h1 class="my-6 text-center text-3xl">오늘은 이 옷 어때?</h1>
+
+            <div class="mt-4 space-y-3 md:space-y-0">
+              <Clothing>
+                <ClothPage />
+                </Clothing>
+            </div>
+          </div>
+                  <div class="mx-6">
+          <h1 class="my-6 text-3xl"></h1>
+          <div class="mx-6 mt-10 grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+            <div class="overflow-hidden rounded-t-md shadow-lg ">
+              <div class="">
+                <img
+                  class="w-sm"
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+                <div class="relative p-2">
+                  <p class="mt-6 text-lg font-semibold">
+                    Basic how to ride your skateboard comfortly
+                  </p>
+                  <p>53K views • 2 weeks ago</p>
+                  <img
+                    class="absolute -top-6 right-6 h-12 w-12 rounded-full border-2 p-0.5"
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="overflow-hidden rounded-t-md shadow-lg">
+              <div class="">
+                <img
+                  class="w-sm"
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+                <div class="relative p-2">
+                  <p class="mt-6 text-lg font-semibold">
+                    Basic how to ride your skateboard comfortly
+                  </p>
+                  <p>53K views • 2 weeks ago</p>
+                  <img
+                    class="absolute -top-6 right-6 h-12 w-12 rounded-full border-2 p-0.5"
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="overflow-hidden rounded-t-md shadow-lg ">
+              <div class="">
+                <img
+                  class="w-sm"
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+                <div class="relative p-2">
+                  <p class="mt-6 text-lg font-semibold">
+                    Basic how to ride your skateboard comfortly
+                  </p>
+                  <p>53K views • 2 weeks ago</p>
+                  <img
+                    class="absolute -top-6 right-6 h-12 w-12 rounded-full border-2 p-0.5"
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="overflow-hidden rounded-t-md shadow-lg ">
+              <div class="">
+                <img
+                  class="w-sm"
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                  alt=""
+                />
+                <div class="relative p-2">
+                  <p class="mt-6 text-lg font-semibold">
+                    Basic how to ride your skateboard comfortly
+                  </p>
+                  <p>53K views • 2 weeks ago</p>
+                  <img
+                    class="absolute -top-6 right-6 h-12 w-12 rounded-full border-2 p-0.5"
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </main>
+      </div>
+      {/* <footer class="text-center py-6 mt-4">
+						Copyright &copy; 2022 dej45.com Template by Namina
+					</footer> */}
     </>
-  )
+  );
 }
+
+const GraphDiv = styled.div`
+  position: absolute;
+  left: 60%;
+  top: 84%;
+  transform: translate(-50%, -20%);
+  margin-left: auto;
+  background-color: #2b90d9;
+  width: 70%;
+  height: 30%;
+  border-radius: 20px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 80%;
+`;
+
+const Clothing = styled.div`
+  padding: 10px;
+  width: 40%;
+  margin-left: 330px;
+  box-sizing: border-box;
+`;
---
+++ src/layouts/Navv.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import LogoImamge from '../assets/images/LOGO.png'
+
+export default function Navv() {
+  return (
+	<div class="md:flex w-2/5 md:w-1/4 h-screen bg-white border-r hidden">
+		<div class="mx-auto py-10">
+        <img src={LogoImamge} alt=""width={"150px"} height={"50px"}  />
+			<ul>
+				<li class="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+					</svg>
+					<span class="font-semibold">Home</span>
+				</li>
+				<li class="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+					</svg>
+					<span class="font-semibold">Feed</span>
+				</li>
+				<li class="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path d="M12 14l9-5-9-5-9 5 9 5z" />
+						<path
+							d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+					</svg>
+					<span class="font-semibold">Chating</span>
+				</li>
+				<li class="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+					</svg>
+					<span class="font-semibold">Profile</span>
+				</li>
+				<button class="w-full mt-10 bg-[#EC5252] rounded-full py-1.5 text-white">Login</button>
+			</ul>
+		</div>
+	</div>  )
+}
